02c3032821ae29614a84069914116fc56f4476d9
Adrian <ochala.adrian@gmail.com>
2019-11-26 19:43:42 +0100

    Menu

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,7 +1,7 @@
 <!-- Nawigacja -->
 <div class="sidenav">
-    <a href="#">About</a>
-    <a href="#">Services</a>
-    <a href="#">Clients</a>
-    <a href="#">Contact</a>
+    <a href="#"> <img src="{{asset("img/fb.png")}}" style="width: 25px;height: 25px;"> <span>Tak</span> </a>
+    <a href="#"><img src="{{asset("img/fb.png")}}" style="width: 25px;height: 25px;"> <span>Services </span> </a>
+    <a href="#"><img src="{{asset("img/fb.png")}}" style="width: 25px;height: 25px;"> <span>Clients </span></a>
+    <a href="#"><img src="{{asset("img/fb.png")}}" style="width: 25px;height: 25px;"> <span>Contact </span></a>
 </div>
