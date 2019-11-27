3e28261ed840d9f4ef65528724c393539bd6f871
Cersin <cersin96@gmail.com>
2019-11-27 21:02:35 +0100

    Stopka

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -16,6 +16,9 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Indie+Flower&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Coming+Soon&display=swap" rel="stylesheet">
+
         <!-- Styles -->
         <link href="{{ URL::asset('css/style.css') }}" rel="stylesheet">
         <script src='https://kit.fontawesome.com/a076d05399.js'></script>
@@ -33,32 +36,37 @@
         @yield('content')
 
         <div class="footer">
+            <div id="social" class="col-lg-12">
+                <h5>Odwiedź nasze Social Media</h5>
+            </div>
             <div class="row ">
                 <div id="footer1" class="col-lg-3">
                     <img src="{{asset("img/transparentIconFooter.png")}}" class="footerIcon"><br>
-                    Speed. Passion. Love.
+                    <h4>Speed. Passion. Love.</h4>
                 </div>
-                <div class="col-lg-2">
-                    <dt>Socials</dt>
-                    <div class="links">
-                    <a href="#"><i class='fab fa-facebook-square'></i></a>
-                    <a href="#"><i class='fab fa-youtube'></i></a>
-                    <a href="#"><i class='fab fa-discord'></i></a>
-                    <a href="#"><i class='fab fa-twitch'></i></a>
+                <div class="col-lg-9">
+                    <div id="social" class="row">
+                        <div class="col-lg-3">
+                            <a href="#"><i class='fab fa-facebook-square'></i></a>
+                            <br>Dołącz do naszej grupy!
+                        </div>
+                        <div class="col-lg-3">
+                            <a href="#"><i class='fab fa-youtube'></i></a>
+                            <br>Zajrzyj na poprzednie wyścigi
+                        </div>
+                        <div class="col-lg-3">
+                            <a href="#"><i class='fab fa-discord'></i></a>
+                            <br>Skontaktuj się z nami lub zapisz do ligi
+                        </div>
+                        <div class="col-lg-3">
+                            <a href="#"><i class='fab fa-twitch'></i></a>
+                            <br>Oglądaj nas na żywo
+                        </div>
                     </div>
-                </div>
-
-                <div class="col-lg-4">
-
-                </div>
-
-                <div class="col-lg-3">
-
                 </div>
             </div>
 
-
-           <small>Copyright by &copy Adrian Ochała & Dawid Szmidt & Szymon Wójcik</small>
+           <h8 id="podpis">Copyright by &copy NameInProgress</h8>
         </div>
     </div>
     </body>
