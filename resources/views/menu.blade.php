dd1a433c497624d2e85bf111a4670a0d10c01e49
Adrian <ochala.adrian@gmail.com>
2019-11-27 16:50:09 +0100

    Icons

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,14 +1,21 @@
 <!-- Nawigacja -->
 <div class="sidenav">
     <img src="{{asset("img/transparentIcon.png")}}" class="menuIcon"><br>
-    <a href="#"> <img src="{{asset("img/fb.png")}}" style="width: 25px;height: 25px;"> <span>Tak</span> </a>
-    <a href="#"><img src="{{asset("img/fb.png")}}" style="width: 25px;height: 25px;"> <span>Services </span> </a>
-    <a href="#"><img src="{{asset("img/fb.png")}}" style="width: 25px;height: 25px;"> <span>Clients </span></a>
-    <a href="#"><img src="{{asset("img/fb.png")}}" style="width: 25px;height: 25px;"> <span>Contact </span></a>
 
-    <a href="#"><i class='fab fa-accessible-icon'></i> <span>Tak</span></a>
-    <a href="#"><i class='far fa-calendar'></i><span>Terminarz</span></a>
-    <a href="#"><i class='fas fa-chalkboard-teacher'></i><span>Społeczność</span></a>
-    <a href="#"><i class='fas fa-chart-line'></i><span>Grafy</span></a>
-    <a href="#"><i class='far fa-folder-open'></i><span>Archiwum</span></a>
+
+    <a href="#"><i class='fab fa-accessible-icon'></i> <span>Aktualności</span></a>
+    <a href="#"><i class='fab fa-accessible-icon'></i> <span>Splity</span></a>
+    <a href="#"><i class='fas fa-info'></i><i class="fa fa-info-circle"></i> <span>Ogłoszenia</span></a>
+    <a href="#"><i class='fas fa-chart-line'></i> <span>Grafy</span></a>
+    <a href="#"><i class='far fa-calendar'></i> <span>Terminarz</span></a>
+    <a href="#"><i class='fas fa-chalkboard-teacher'></i> <span>Społeczność</span></a>
+    <a href="#"><i class='far fa-folder-open'></i> <span>Archiwum</span></a>
+
+<div class="links">
+    <a href="#"><i class='fab fa-facebook-square'></i></a>
+    <a href="#"><i class="fa fa-youtube"></i></a>
+    <a href="#"><i class='fab fa-youtube'></i></a>
+    <a href="#"><i class='fab fa-discord'></i></a>
+    <a href="#"><i class='fab fa-twitch'></i></a>
+</div>
 </div>
