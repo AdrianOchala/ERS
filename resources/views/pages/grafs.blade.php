e8d9593ab4627488b77799995f1e914d0909071e
Cersin <cersin96@gmail.com>
2019-11-28 17:04:00 +0100

    excel

--- resources/views/pages/grafs.blade.php
+++ resources/views/pages/grafs.blade.php
@@ -1,6 +1,12 @@
 @extends('template')
 @section('content')
 
-
    <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vT_PCw1rQjXuYr-bPnGqCzlceY5Ve6fp-tF6b7Oi5KYNBqHpkAsJz8AvX-wyUpw0is7hm5hKk7qZWM7/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
+
+   {{--<iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vT_PCw1rQjXuYr-bPnGqCzlceY5Ve6fp-tF6b7Oi5KYNBqHpkAsJz8AvX-wyUpw0is7hm5hKk7qZWM7/pubhtml?gid=354051947&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
+
+   <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vT_PCw1rQjXuYr-bPnGqCzlceY5Ve6fp-tF6b7Oi5KYNBqHpkAsJz8AvX-wyUpw0is7hm5hKk7qZWM7/pubhtml?gid=69261495&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
+
+   <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vT_PCw1rQjXuYr-bPnGqCzlceY5Ve6fp-tF6b7Oi5KYNBqHpkAsJz8AvX-wyUpw0is7hm5hKk7qZWM7/pubhtml?gid=234316779&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
+--}}
 @stop
