e1f0147af8ab0f1194a6127134587b6c430fd551
Cersin <cersin96@gmail.com>
2020-01-14 18:55:21 +0100

    ads

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -3,9 +3,8 @@
     <img src="{{asset("img/transparentIcon.png")}}" class="menuIcon"><br>
 
 
-    <a href="{{asset('/')}}"><i class='fab fa-accessible-icon'></i> <span>Aktualności</span></a>
+    <a href="{{asset('/')}}"><i class='fas fa-info'></i> <span>Aktualności</span></a>
     <a href="{{asset('/splits')}}"><i class='fab fa-accessible-icon'></i> <span>Splity</span></a>
-    <a href="{{asset('/ads')}}"><i class='fas fa-info'></i><i class="fa fa-info-circle"></i> <span>Ogłoszenia</span></a>
     <a href="{{asset('/grafs')}}"><i class='fas fa-chart-line'></i> <span>Grafy</span></a>
     <a href="{{asset('/calendar')}}"><i class='far fa-calendar'></i> <span>Terminarz</span></a>
     <a href="{{asset('/community')}}"><i class='fas fa-chalkboard-teacher'></i> <span>Społeczność</span></a>
