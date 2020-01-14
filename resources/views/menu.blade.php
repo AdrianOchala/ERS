a7dfd2a9b52840563a365997c0f38732bd48e10c
Adrian <ochala.adrian@gmail.com>
2020-01-14 16:30:36 +0100

    Haloo

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -3,7 +3,7 @@
     <img src="{{asset("img/transparentIcon.png")}}" class="menuIcon"><br>
 
 
-    <a href="{{asset('/home')}}"><i class='fab fa-accessible-icon'></i> <span>Aktualności</span></a>
+    <a href="{{asset('/')}}"><i class='fab fa-accessible-icon'></i> <span>Aktualności</span></a>
     <a href="{{asset('/splits')}}"><i class='fab fa-accessible-icon'></i> <span>Splity</span></a>
     <a href="{{asset('/ads')}}"><i class='fas fa-info'></i><i class="fa fa-info-circle"></i> <span>Ogłoszenia</span></a>
     <a href="{{asset('/grafs')}}"><i class='fas fa-chart-line'></i> <span>Grafy</span></a>
