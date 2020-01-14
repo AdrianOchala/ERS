43f4aaa19b7762042a87f1ee317afd92f718ba06
Cersin <cersin96@gmail.com>
2020-01-14 18:58:35 +0100

    split

--- resources/views/pages/splits.blade.php
+++ resources/views/pages/splits.blade.php
@@ -1,19 +1,9 @@
 @extends('template')
 @section('content')
 <div class="splits">
-    <h2>Splity</h2>
-
-    Esport Racing Series
-
-
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sodales pulvinar leo, in tincidunt magna rutrum id. Nam nisl sem, ornare sed risus quis, aliquet lobortis metus. Cras vitae nibh massa. Cras nec dolor non neque condimentum vulputate in tincidunt augue. Pellentesque molestie lobortis enim, non rhoncus est porttitor at. In eleifend iaculis blandit. Vivamus dapibus neque a nulla congue, vitae tempor nunc ultricies. Phasellus sit amet libero diam. Donec consectetur viverra faucibus. Donec quis sem ut arcu suscipit sagittis vel at metus. Curabitur tincidunt id quam vitae facilisis.
-
-    Duis tincidunt, augue eu cursus lobortis, leo tellus bibendum sapien, in tincidunt nunc purus non magna. Donec non consequat turpis. Proin sit amet sapien est. Etiam vel felis auctor, mollis enim non, ornare ex. Integer enim ex, sagittis in fermentum eget, placerat eget sem. Sed vitae fringilla sem, eu sollicitudin nisl. Sed at interdum turpis. Mauris iaculis ex nec sagittis hendrerit. Maecenas pretium tempor neque, vitae sagittis nulla vulputate et. Quisque posuere, velit nec porttitor ornare, tellus ligula posuere tortor, nec varius lectus ipsum vel augue. Sed luctus volutpat ex, at luctus nunc pretium sit amet. Nunc ut porttitor magna, in tempor eros. Aliquam massa massa, tincidunt et purus eget, fringilla sollicitudin nulla. Nullam convallis tellus eget massa auctor, ut pretium augue ornare.
-
-    Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed dignissim, nisi vitae faucibus consequat, erat quam gravida lorem, ut sollicitudin velit arcu in risus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla est mi, hendrerit in molestie nec, gravida at urna. Vestibulum elementum mi a viverra pretium. Etiam vitae turpis eget ligula elementum ullamcorper quis nec augue. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque et nunc elit. In hac habitasse platea dictumst. Phasellus blandit, nunc in aliquam feugiat, ex erat consequat tortor, et pharetra metus purus at nulla. Praesent et ex condimentum nibh aliquam eleifend vel a sapien. Phasellus eget ultricies arcu.
-
-    Integer commodo blandit lorem, at ornare velit. Integer egestas euismod dignissim. Nam congue ac urna et luctus. Cras varius pellentesque nibh, in interdum arcu ornare at. Cras condimentum augue eu accumsan imperdiet. Etiam eget consequat augue, vitae luctus tellus. Etiam rutrum diam eget velit efficitur, vitae efficitur felis faucibus. In a scelerisque velit, a scelerisque ex. In at orci ac dui semper commodo quis in quam. In eget nibh pharetra, ornare ligula vel, ultricies lacus. Curabitur placerat faucibus est eget eleifend. Maecenas non libero a augue finibus feugiat vel sit amet orci. Integer dui ex, lobortis non commodo vitae, scelerisque a nulla. Aliquam mollis massa et risus convallis posuere.
-
-    Curabitur augue arcu, mollis id diam sollicitudin, interdum pellentesque lorem. Cras et cursus diam. Aliquam at ultricies arcu. Sed consequat enim et nulla faucibus sagittis. Vestibulum arcu nunc, sollicitudin in cursus id, finibus sit amet ipsum. Nam sagittis tincidunt purus, ut porttitor nibh sagittis non. Mauris vitae velit eu est porta accumsan. Duis eu eros porttitor, pellentesque urna ac, sodales ante. Nulla accumsan, dui sit amet pharetra vestibulum, neque lorem viverra nulla, quis viverra lectus ipsum ut diam. Maecenas ultricies, turpis sit amet rutrum feugiat, massa dolor sollicitudin orci, eget commodo dolor nulla ut ex. Nunc a pretium ligula, vel congue orci. Donec id hendrerit elit.
+    <h3>In progress</h3>
+    <h2>Split 1</h2>
+    <h2>Split 2</h2>
+    <h2>Split 3</h2>
 </div>
 @stop
