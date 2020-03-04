14196dc4567e99019f056b9a65cbd10c5cf5b5d9
Cersin <cersin96@gmail.com>
2020-03-04 21:07:48 +0100

    bugs

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -27,7 +27,7 @@
         <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v5.0"></script>
     </head>
     <body>
-    <div class="wrapper content ">
+    <div class="wrapper content">
 
 
         <div class="header">
