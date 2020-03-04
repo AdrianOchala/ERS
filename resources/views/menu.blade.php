6202599a009f26650e52824d64723d8642f8a0c7
Cersin <cersin96@gmail.com>
2020-03-04 21:53:51 +0100

    bugs

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -7,7 +7,7 @@
     <a href="{{asset('/splits')}}"><i class='fab fa-accessible-icon'></i> <span>Splity</span></a>
     <a href="{{asset('/grafs')}}"><i class='fas fa-chart-line'></i> <span>Grafy</span></a>
     <a href="{{asset('/calendar')}}"><i class='far fa-calendar'></i> <span>Terminarz</span></a>
-    <a href="{{asset('/archives')}}"><i class='far fa-folder-open'></i> <span>Archiwum</span></a>
+    <a href="{{asset('/ersGreen')}}"><i class='far fa-folder-open'></i> <span>Archiwum</span></a>
 
 </div>
 
@@ -17,7 +17,7 @@
     <a href="{{asset('/splits')}}">Splity</a>
     <a href="{{asset('/grafs')}}">Grafy</a>
     <a href="{{asset('/calendar')}}">Terminarz</a>
-    <a href="{{asset('/archives')}}">Archiwum</a>
+    <a href="{{asset('/ersGreen')}}">Archiwum</a>
 
 
 
